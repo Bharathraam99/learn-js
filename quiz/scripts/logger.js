64c5c6ff7e1560802381b7e13d4da3bfc1145ecf
Bharath Raam <bharathsrini19@gmail.com>
2024-01-29 17:52:21 -0500

    012924 Activity (Jan 25, 26)

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,18 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    if (this && this.hasOwnProperty('errMsg')) {
+        console.error(this.errMsg);
+    } else {
+        console.log("No error message found in the object.");
+    }
+}
+
+const exampleObject1 = {
+    errMsg: "This is an error message for testing logMsg function."
+};
+const exampleObject2 = {
+    testMsg: "This is an test message for testing logMsg function."
+};
+
+logMsg.call(exampleObject1);
+logMsg.call(exampleObject2);
