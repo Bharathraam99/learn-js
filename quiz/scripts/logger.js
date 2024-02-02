ca30df73c60fd0a5c68192a0f3b3451cb144564b
Bharath Raam <bharathsrini19@gmail.com>
2024-02-01 21:28:32 -0500

    Jan 30 Activity JS

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,20 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    if(this.errMsg){
+        console.log('Error Message: '+this.errMsg);
+    }
+    else{
+        console.log('No Error Message Found');
+    }
+}
+
+const withError = {
+    errMsg: "THIS IS AN ERROR MESSAGE"
+}
+
+const withoutError = {
+    noErrMsg: "THIS IS NOT AN ERROR MESSAGE"
+}
+
+logMsg.call(withError)
+logMsg.call(withoutError)
