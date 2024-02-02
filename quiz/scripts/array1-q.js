fe9d08bd4e354f0f1555453c2b3af5fa06c39bad
Bharath Raam <bharathsrini19@gmail.com>
2024-02-02 18:48:26 -0500

    Feb 1 Activity

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -1,32 +1,38 @@
-window.onload = function() {
-    const list = document.getElementById('Top5');
-    const inp = document.getElementById('inp');
-    const btn = document.getElementById('btn');
-    list.innerHTML = '';
+window.onload = function () {
+  const list = document.getElementById("Top5");
+  const inp = document.getElementById("inp");
+  const btn = document.getElementById("btn");
+  list.innerHTML = "";
 
-    const myHistory = [];
-    const MAX_HISTORY = 5;
+  const myHistory = [];
+  const MAX_HISTORY = 5;
 
-    btn.onclick = () => {
+  btn.onclick = () => {
     // we will only allow a term to be entered if the search input isn't empty
-      if (inp.value !== '') {
-              // empty the list so that we don't display duplicate entries
-              // the display is regenerated every time a search term is entered.
-              list.innerHTML = '';
+    if (inp.value !== "") {
+      // empty the list so that we don't display duplicate entries
+      // the display is regenerated every time a search term is entered.
+      list.innerHTML = "";
+      myHistory.unshift(inp.value);
+      let myHistoryCopy = [...myHistory];
+      myHistoryCopy.sort(function (a, b) {
+        return a.length - b.length;
+      });
+      // loop through the sorted array, and display all the search terms in the list
+      for (const itemText of myHistoryCopy) {
+         const listItem = document.createElement("li");
+         listItem.textContent = itemText
+         list.appendChild(listItem);
+      }
 
-              // loop through the sorted array, and display all the search terms in the list
-              for (const itemText of myHistoryCopy) {
-                
-              }
+      // If the array length is 5 or more, remove the oldest search term
+      if (myHistory.length >= MAX_HISTORY) {
+        myHistory.pop()
+      }
 
-              // If the array length is 5 or more, remove the oldest search term
-              if (myHistory.length >= MAX_HISTORY) {
-                
-              }
-
-              // empty the search input and focus it, ready for the next term to be entered
-              inp.value = '';
-              btn.focus();
-          }
+      // empty the search input and focus it, ready for the next term to be entered
+      inp.value = "";
+      btn.focus();
     }
-}
+  };
+};
--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,29 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createButtonNode(txt, e, l){
+  let buttonNode = document.createElement("button");
+  buttonNode.textContent = txt;
+  buttonNode.addEventListener(e, l);
+  return buttonNode;
+}
+
+function edit(node){
+  let newnode = document.createElement("input");
+  newnode.type ="text"
+  newnode.placeholder="Enter Cell (x,y) ..."
+  node.replaceChild(newnode, node.childNodes[0]);
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createButtonNode("Edit Text", "click", function(){
+      console.log('dsg');
+      edit(col1)
+    }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
